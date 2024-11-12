9c0517a59a52d60c6a25662025e6e7ab7da74df1
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-11-12 06:44:53 -0400

    Commit inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+function calcularMedia(a, b, c) {
+    return (a + b + c) / c; 
+}
+
+function exibirResultado() {
+    const numero1 = parseFloat(document.getElementById("numero1").value);
+    const numero2 = parseFloat(document.getElementById("numero2").value);
+    const numero3 = parseFloat(document.getElementById("numero3").value);
+
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) {
+        alert("Por favor, insira valores válidos!");
+        return;
+    }
+
+    const media = calcularMedia(numero1, numero2, numero3);
+    document.getElementById("resultadoMedia").innerText = `A média dos números é: ${media.toFixed(2)}`;
+}
+
+document.getElementById("calcularBtn").addEventListener("click", exibirResultado);
